13b419ba52ba37af99f69354c44eef3d5755441c
Tomohiro <32695191+cotton392@users.noreply.github.com>
2021-06-18 10:46:20 +0900

    Update top.vue

---
+++ front/top.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>ここいじって欲しい</h1>
+</template>
+
+<script>
+module.exports = {
+  data: function() {
+    return {
+      title: 'top.vue'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+    color: red;
+}
+</style>
