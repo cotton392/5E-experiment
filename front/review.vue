0a59f4d6e7cd74ff62ad6a37c42f2d15cfdd0405
Tomohiro <32695191+cotton392@users.noreply.github.com>
2021-06-18 10:47:36 +0900

    Update review.vue

---
+++ front/review.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1ここ弄って欲しい</h1>
+</template>
+
+<script>
+module.exports = {
+  data: function() {
+    return {
+      title: 'review.vue'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+    color: blue;
+}
+</style>
