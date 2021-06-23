e46939d4256658da94fa4c0d6a5e3caf255117b1
heroinx <e1705@s.akashi.ac.jp>
2021-06-23 10:12:28 +0900

    modified front

--- front/main.js
+++ front/main.js
@@ -1,11 +1,11 @@
 const router = new VueRouter({
     routes: [
-        { path: '/', component: httpVueLoader('./top.vue') },
-        { path: '/review', component: httpVueLoader('./review.vue') },
+        { path: 'https://172.16.16.7:8086/team6/', component: httpVueLoader('./top.vue') },
+        { path: 'https://172.16.16.7:8086/team6/review', component: httpVueLoader('./review.vue') },
     ]
 })
 
 const app = new Vue({
     el: "#app",
-    router
-});
+    router, 
+}).$mount('#app');
--- front/review.vue
+++ front/review.vue
@@ -1,19 +1,40 @@
 <template>
-  <h1ここ弄って欲しい</h1>
+  <div id="app">
+        <h1>ここ弄って欲しい</h1>
+        <input type="text" v-model="message">
+        <div class="error" v-if="error.require">必須項目です。</div>
+        <div class="error" v-if="error.tooLong">長すぎます。</div>
+        <div class="error" v-if="error.tooShort">短すぎます。</div>
+  </div>
 </template>
 
 <script>
 module.exports = {
   data: function() {
     return {
-      title: 'review.vue'
+      title: 'review.vue',
+      message: 'Hello',
+        error: {
+            require: false,
+            tooLong: false,
+            tooShort: false
+        }
+    }
+  },
+  methods: {
+  },
+  watch :{
+    message: function(newVal, oldVal) {
+        this.error.require = (newVal.length < 1) ? true : false;
+        this.error.tooLong = (newVal.length > 255) ? true : false;
+        this.error.tooShort = (newVal.length < 4) ? true : false;
     }
   }
 }
 </script>
 
 <style scoped>
-h1 {
-    color: blue;
+#hoge.error {
+        color: red;
 }
 </style>
