64e144aa346fbe5fa4315b70462f4a38fa042eb8
heroinx <e1705@s.akashi.ac.jp>
2021-07-07 09:06:05 +0900

    レビュー

--- front/review.vue
+++ front/review.vue
@@ -5,7 +5,18 @@
         <div class="error" v-if="error.require">必須項目です。</div>
         <div class="error" v-if="error.tooLong">長すぎます。</div>
         <div class="error" v-if="error.tooShort">短すぎます。</div>
+ <!--        
+<select v-model="selectedMenues">
+  <option disabled value="">メニュー一覧</option>
+  <option v-for="menu in optionMenues" 
+    v-bind:value="menu.name" 
+    v-bind:key="menu.id">
+  {{ Menu.name }}
+  </option>
+</select>
+-->
   </div>
+  
 </template>
 
 <script>
@@ -19,6 +30,13 @@ module.exports = {
             tooLong: false,
             tooShort: false
         }
+      
+/*      selectedMenues: '',
+      optionMenues: [
+        {id: 1, name: 'Aセット'},
+        {id: 2, name: 'Bセット'},
+        {id: 3, name: 'その他これ増やす'}
+      ],*/
     }
   },
   methods: {
@@ -34,7 +52,7 @@ module.exports = {
 </script>
 
 <style scoped>
-#hoge.error {
+#app.error {
         color: red;
 }
 </style>
